eb6539bbc1bf37b5904aac3798eda17edb0d12ba
root <root@ip-10-1-0-223.ap-south-1.compute.internal>
2024-04-22 19:40:10 +0530

    CAT voting and comments

---
+++ sites/all/themes/tweme/templates/comment.tpl.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @file
+ * Bartik's theme implementation for comments.
+ *
+ * Available variables:
+ * - $author: Comment author. Can be link or plain text.
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $created: Formatted date and time for when the comment was created.
+ *   Preprocess functions can reformat it by calling format_date() with the
+ *   desired parameters on the $comment->created variable.
+ * - $changed: Formatted date and time for when the comment was last changed.
+ *   Preprocess functions can reformat it by calling format_date() with the
+ *   desired parameters on the $comment->changed variable.
+ * - $new: New comment marker.
+ * - $permalink: Comment permalink.
+ * - $submitted: Submission information created from $author and $created during
+ *   template_preprocess_comment().
+ * - $picture: Authors picture.
+ * - $signature: Authors signature.
+ * - $status: Comment status. Possible values are:
+ *   comment-unpublished, comment-published or comment-preview.
+ * - $title: Linked title.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the following:
+ *   - comment: The current template type, i.e., "theming hook".
+ *   - comment-by-anonymous: Comment by an unregistered user.
+ *   - comment-by-node-author: Comment by the author of the parent node.
+ *   - comment-preview: When previewing a new or edited comment.
+ *   The following applies only to viewers who are registered users:
+ *   - comment-unpublished: An unpublished comment visible only to administrators.
+ *   - comment-by-viewer: Comment by the user currently viewing the page.
+ *   - comment-new: New comment since last the visit.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * These two variables are provided for context:
+ * - $comment: Full comment object.
+ * - $node: Node object the comments are attached to.
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_comment()
+ * @see template_process()
+ * @see theme_comment()
+ */
+?>
+<?php 
+	$user_id = $comment->uid;
+	//print $user_id;
+	$user_obj = user_load($user_id);
+	$roles = $user_obj->roles;
+	if (in_array("AT Portal", $roles))
+	{
+		$at_name = db_query("select CONCAT(t_f_name, ' ', t_l_name) from dh_teacher where t_status='Active' and CONCAT(t_code,'.',t_gender) = '".$user_obj->name."'")->fetchField();
+		if ($at_name)
+			$at_name = "( $at_name )";
+	}
+
+
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="comment-text">
+    <div class="comment-arrow"></div>
+
+    <div class="content"<?php print $content_attributes; ?>>
+	<b><hr>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['links']);
+        print render($content);
+	?>
+	</b>
+    </div> <!-- /.content -->
+    
+  </div> <!-- /.comment-text -->
+
+  <div class="attribution">
+    <div class="submitted">
+      <p class="comment-time">
+        <?php print $author; ?> <?php print $at_name; ?> ,  <?php print $created; ?>, <?php print render($content['links']); ?>
+      </p>
+    </div>
+  </div>
+</div>
