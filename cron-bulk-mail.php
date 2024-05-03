831ff39d216addcfff8857dc8c6f247a8acca3ee
vastavanand <golushailuoo7@gmail.com>
2024-05-03 16:35:49 +0530

    bulk mail edits

--- cron-bulk-mail.php
+++ cron-bulk-mail.php
@@ -23,16 +23,18 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-$q = "select bm_id, bm_query, bm_letter from dh_bulk_mail where DATE_FORMAT(bm_schedule_date, '%Y-%m-%d') = CURDATE() and bm_processed=0";
+$q = "select bm_id, bm_query, bm_letter from dh_bulk_mail where DATE_FORMAT(bm_schedule_date, '%Y-%m-%d') = CURDATE() and bm_processed=0 and bm_deleted=0";
 $result = db_query($q);
 while ($row = $result->fetchAssoc()) 
 {
    echo "Processing Bulk Mail ID ".$row['bm_id']."\n";
+   $q = "update dh_bulk_mail set bm_processed=2 where bm_id=".$row['bm_id'];
+   db_query($q);
    $result2 = db_query($row['bm_query']);
    while ($row2 = $result2->fetchAssoc()) 
    {
       echo "Pushing ".$row2['a_id']."\n";
-      $data = array('applicant_id' => $row2['a_id'], 'letter_id' => $row['bm_letter']);
+      $data = array('bulk_mail_id' => $row['bm_id'], 'applicant_id' => $row2['a_id'], 'letter_id' => $row['bm_letter']);
       push_to_queue( 'Dipi', 'mail', json_encode( $data ), 86400000);
    }
    $q = "update dh_bulk_mail set bm_processed=1 where bm_id=".$row['bm_id'];
