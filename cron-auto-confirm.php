fadc5d0112d24b33ae82e786f81816568cdae788
root <root@ip-10-1-0-223.ap-south-1.compute.internal>
2024-06-30 11:21:16 +0530

    auto-confirm-pagoda

--- cron-auto-confirm.php
+++ cron-auto-confirm.php
@@ -28,7 +28,7 @@ db_query("update dh_course set c_auto_confirm=1 where c_center=308 and c_start>c
 
 // auto confirming/rejecting applications for courses set to autoconfirm on
 
-$q = "select a_id, a_course, a_f_name, a_l_name, a_email, a_status from dh_applicant a left join dh_course c on (a.a_course=c.c_id) where c.c_start >= CURDATE() and c.c_auto_confirm=1 and a.a_status in ('Received') and a_type='Student' limit 50";
+$q = "select a_id, a_course, a_f_name, a_l_name, a_email, a_status, a_dob, a_zip from dh_applicant a left join dh_course c on (a.a_course=c.c_id) where c.c_start >= CURDATE() and c.c_auto_confirm=1 and a.a_status in ('Received') and a_type='Student' limit 50";
 $result = db_query($q);
 while ($row = $result->fetchAssoc()) 
 {
@@ -38,7 +38,28 @@ while ($row = $result->fetchAssoc())
    try 
    {
       if ($ret <= 0)
+      {
+	      echo "inside ret <= 0\n";
+        if ($row['a_email'] <> '')
+          $append = " and a_email='".$row['a_email']."'";
+        elseif ($row['a_zip'] <> '')
+          $append = " and a_zip='".$row['a_zip']."'";
+        else
+          $append = " and a_dob='".$row['a_dob']."'";
+
+	echo "append: $append\n";
+        $attended = db_query("select count(*) from dh_applicant left join dh_course on a_course=c_id left join dh_type_detail on td_id=c_course_type where a_f_name=:a_f_name and a_l_name=:a_l_name and td_val3 not in ('1-Day') and a_attended='1' $append", array(':a_f_name' => $row['a_f_name'], ':a_l_name' => $row['a_l_name']))->fetchField();
+	echo "attended: $attended\n";
+        if($attended >=1)
+        {
          update_status_external($row['a_id'], 'Confirmed');
+        }
+        else
+        {
+          _update_status($row['a_id'], "Clarification");
+          dh_send_letter('applicant', $row['a_id'], "Clarification", "7933");
+        }
+      }
       else
       {
          update_status_external($row['a_id'], 'Rejected');
