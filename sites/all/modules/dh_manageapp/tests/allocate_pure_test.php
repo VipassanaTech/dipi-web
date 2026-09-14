211ac085f20ba3ef69f8029036518a3fbf46c73c
vastavanand <golushailuoo7@gmail.com>
2026-09-14 12:55:50 +0530

    feat(alloc): pool builder (combined / old-new split / reserved)

--- sites/all/modules/dh_manageapp/tests/allocate_pure_test.php
+++ sites/all/modules/dh_manageapp/tests/allocate_pure_test.php
@@ -17,4 +17,16 @@ ok(dh_alloc_expand('1-3, 5') === array('1','2','3','5'), 'expand mixed');
 ok(dh_alloc_expand('A1-A3') === array('A1','A2','A3'), 'expand letter range');
 ok(dh_alloc_expand('1, , 2') === array('1','2'), 'expand skips blanks');
 
+// Task 2 tests - pool
+$ini = array(
+  'MALE'   => array('Cells'=>'1-4','Reserved'=>'2'),
+  'FEMALE' => array('Old'=>'1-2','New'=>'3-4','Reserved'=>''),
+);
+$pm = dh_alloc_pool($ini,'MALE');
+ok($pm['sep']===false && $pm['cells']===array('1','2','3','4') && $pm['reserved']===array('2'), 'pool combined');
+$pf = dh_alloc_pool($ini,'FEMALE');
+ok($pf['sep']===true && $pf['old']===array('1','2') && $pf['new']===array('3','4'), 'pool split');
+$pmiss = dh_alloc_pool($ini,'MALE_1');
+ok($pmiss['cells']===array() && $pmiss['old']===array() && $pmiss['reserved']===array(), 'pool missing section');
+
 echo empty($GLOBALS['fail']) ? "ALL PASS\n" : "FAILURES\n";
